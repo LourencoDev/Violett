9ff301a61ba05797ad0cf54995ad0dec08a13f1c
LourencoDev <51246899+LourencoDev@users.noreply.github.com>
2021-04-13 22:49:30 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
-const api = {
-  criadorCadastrar: axios.create({
-    baseURL: "http://localhost:3000/criador/cadastrar";
-  }),
-  criadorLogin: axios.create({
-    baseURL: "http://localhost:3000/criador/login";
-  }),
-  recrutadorCadastrar: axios.create({
-    baseURL: "http://localhost:3000/recrutador/cadastrar";
-  }),
-  recrutadorLogin: axios.create({
-    baseURL: "http://localhost:3000/recrutador/login";
-  }),
-}
+const api = axios.create({
+  baseURL: "http://localhost:3000";
+});
 
 export default api;
